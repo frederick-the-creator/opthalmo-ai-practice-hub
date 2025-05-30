ebf9aa5e5b826b8dc1ff502e46b4e1ff1318c442
frederick-the-creator <frederickl@hotmail.co.uk>
2025-05-30 16:42:10 +0100

    Update CORS to allow requests from FRONTEND URL

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+console.log('FRONTEND_URL:', FRONTEND_URL);
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 app.use(cors({
