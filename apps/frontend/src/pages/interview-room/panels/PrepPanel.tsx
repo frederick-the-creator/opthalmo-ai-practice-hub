81a5cff4f2f9164d0dd277248017016295f4d6b8
Frederick Lewis <frederickl@hotmail.co.uk>
2025-08-08 22:25:02 +0100

    Change select case scroll

--- apps/frontend/src/pages/interview-room/panels/PrepPanel.tsx
+++ apps/frontend/src/pages/interview-room/panels/PrepPanel.tsx
@@ -57,13 +57,7 @@ const PrepPanel: React.FC<PrepPanelProps> = ({ session, cases, role, updating, o
 
   // Case selection content
   const caseContent = (
-    <div
-      className="flex flex-col"
-      style={{
-        maxHeight: 300,
-        overflowY: "auto",
-      }}
-    >
+    <div className="flex flex-col">
       {cases.map(c => {
         // console.log('case:', c);
         return (
@@ -95,22 +89,28 @@ const PrepPanel: React.FC<PrepPanelProps> = ({ session, cases, role, updating, o
   return (
     <div className="ml-5 w-[462px] flex-shrink-0 max-md:ml-0 max-md:w-full">
       <div className="flex flex-col h-[calc(100vh-14rem)] max-md:mt-10 max-md:max-w-full">
-        <Brief
-          title="Please confirm who will be the candidate"
-          markdown={null}
-          placeholder={''}
-          defaultOpen={true}
-        >
-          {role === 'host' ? candidateContent : guestCandidatePlaceholder}
-        </Brief>
-        <Brief
-          title="Please select your case"
-          markdown={null}
-          placeholder={''}
-          defaultOpen={true}
-        >
-          {role === 'host' ? caseContent : guestCasePlaceholder}
-        </Brief>
+        <div className="flex-1 mt-2 max-w-full text-black w-[462px] flex flex-col min-h-0">
+          <div className="flex-none">
+            <Brief
+              title="Please confirm who will be the candidate"
+              markdown={null}
+              placeholder={''}
+              defaultOpen={true}
+            >
+              {role === 'host' ? candidateContent : guestCandidatePlaceholder}
+            </Brief>
+          </div>
+          <div className="flex-1 min-h-0 flex flex-col">
+            <Brief
+              title="Please select your case"
+              markdown={null}
+              placeholder={''}
+              defaultOpen={true}
+            >
+              {role === 'host' ? caseContent : guestCasePlaceholder}
+            </Brief>
+          </div>
+        </div>
         {/* Start Case button moved to InterviewControls */}
       </div>
     </div>
